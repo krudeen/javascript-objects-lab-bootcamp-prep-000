3877dddfc5bf1e4e0ec64626e87bb327ddf6d515
Kyle Rudeen <krudeen@gmail.com>
2018-07-31 19:49:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,45 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+<<<<<<< HEAD
   object[key] = value;
+=======
+  object[key] = value
+>>>>>>> 658aae2e030eac4ec27eac2460d2198e46aa2ae2
   return object
 }
  
 function deleteFromObjectByKey(object, key){
+<<<<<<< HEAD
   var newObj = Object.assign({ }, object)
+=======
+  var newObj = Object.assign({}, obj)
+>>>>>>> 658aae2e030eac4ec27eac2460d2198e46aa2ae2
   delete newObj[key];
   return newObj;
   
 }
 
+<<<<<<< HEAD
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
+=======
+function deleteFromObjectByKey(object, key){
+  delete key.object;
+  return object;
+}
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+    return object;
+}
+    
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object
+}
+>>>>>>> 658aae2e030eac4ec27eac2460d2198e46aa2ae2
